e2715683630e2c0d53a024c79a500ee5c47faa42
Laszlo Somogyi <laszlo.somogyi@gmail.com>
2023-04-27 09:29:25 +0100

    First commit Starter code

---
+++ script.js
@@ -0,0 +1,63 @@
+var body = document.body;
+var h1El = document.createElement("h1");
+var infoEl = document.createElement("div");
+var imgEl = document.createElement("img");
+var kittenEl = document.createElement("div");
+var nameEl = document.createElement("div");
+var favoriteEl = document.createElement("div");
+// Create ordered list element
+var listEl = document.createElement("ol");
+// Create ordered list items
+var li1 = document.createElement("li");
+var li2 = document.createElement("li");
+var li3 = document.createElement("li");
+var li4 = document.createElement("li");
+
+h1El.textContent = "Welcome to my page";
+kittenEl.textContent = "This is my kitten 🐱.";
+nameEl.textContent = "His name is Jax.";
+favoriteEl.textContent = "My favorite foods are:";
+
+body.appendChild(h1El);
+body.appendChild(infoEl);
+infoEl.appendChild(imgEl);
+infoEl.appendChild(kittenEl);
+infoEl.appendChild(nameEl);
+body.appendChild(favoriteEl);
+favoriteEl.appendChild(listEl);
+// Append ordered list 
+favoriteEl.appendChild(listEl);
+
+h1El.setAttribute("style", "margin:auto; width:50%; text-align:center;");
+infoEl.setAttribute("style", "margin:auto; width:50%; text-align:center;");
+imgEl.setAttribute("src", "http://placekitten.com/200/300");
+nameEl.setAttribute("style", "font-size:25px; text-align:center;");
+kittenEl.setAttribute("style", "font-size:25px; text-align:center;");
+favoriteEl.setAttribute("style", "font-size:20px;");
+
+// TODO: Add ordered list items containing four favorite foods
+li1.textContent = "Apples";
+li2.textContent = "Pizza";
+li3.textContent = "Dumplings";
+li4.textContent = "Cupcakes";
+
+listEl.appendChild(li1);
+listEl.appendChild(li2);
+listEl.appendChild(li3);
+listEl.appendChild(li4);
+
+//listEl.setAttribute("style", "backgroundColor=#333333; padding:20px");
+listEl.style.backgroundColor="#333333";
+listEl.style.padding="20px";
+
+allListElement = document.querySelectorAll("li");
+for(var i=0; i<allListElement.length; i++) {
+  //allListElement[i].setAttribute("style", "color=white; padding:5px; margin-left=35px");
+  allListElement[i].style.color = "white";
+  allListElement[i].style.padding = "5px";
+  allListElement[i].style.marginLeft = "35px";
+}
+li1.style.backgroundColor="blue";
+li2.style.backgroundColor="green";
+li3.style.backgroundColor="grey";
+li4.style.backgroundColor="black";
